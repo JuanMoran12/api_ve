14bd037973e6b256c48230ab3dd3b19704f77b33
juan <juanpablomoranperez@gmail.com>
2026-05-07 19:34:06 -0400

    new action

--- scraper/scrap.py
+++ scraper/scrap.py
@@ -4,7 +4,6 @@ from datetime import datetime
 #from playwright.sync_api import sync_playwright
 import random
 import asyncio
-from apscheduler.schedulers.background import BackgroundScheduler
 import logging
 import os
 import re
